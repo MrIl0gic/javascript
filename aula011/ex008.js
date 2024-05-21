67f0695201bed4f9af9415c32949fa7503e3d627
Calvin V. <90356360+MrIl0gic@users.noreply.github.com>
2024-05-21 16:20:58 -0300

    aula 8-9 Condições simples

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 70
+if(velocidade > 60)
+    {
+    console.log("Você passou a velocidade permitida. Multado!") 
+    }   
+    else{
+        console.log("Esta dentro do limite de velociadade, siga com segurança")
+         }
